e8c9f889ce1db26d08b3cfaaa36cb3542472bf0b
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-06-19 14:24:06 +0100

    update refator

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
 from typing import Text
 from kivymd.app import MDApp
 from kivymd.uix.floatlayout import MDFloatLayout
-from kivy.uix.screenmanager import ScreenManager,Screen
+from kivy.uix.screenmanager import Screen,ScreenManager
 from kivy.core.window import Window
 
 
 Window.size=(320,600)
 
-class Gerenciador(ScreenManager):
-    pass
-
-class Fundo_Login(Screen):
-    pass
-
-class Fundo_Register(Screen):
-    pass
-
 class TelaLogin(Screen):
-    def open_register(self):
-        #self.add_widget(Fundo_Register())
-        self.remove_widget(Fundo_Login())
-
+    pass
 class TelaRegister(Screen):
-   def open_login(self):
-    self.add_widget(Fundo_Login())
+    pass
 
 
 class Main(MDApp):
     def build(self):
-        return Gerenciador()
+        return super().build()
 
 
 Main().run()
